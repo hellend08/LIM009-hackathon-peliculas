58013dec2e0ced4bc7243cc861ea370ab397a81b
Hellend08 <sccp.hvasquez@gmail.com>
2019-04-16 03:28:28 -0500

    actulizando funcion de ordenado

--- src/data.js
+++ src/data.js
@@ -1,6 +1,25 @@
-const sorData = (a, b) => {// Función para ordenar los pelicula por nombre  
-  (a.Title > b.Title ? 1 : -1);   
-};
+// const sorData = (a, b) => {// Función para ordenar los pelicula por nombre  
+//   (a.Title > b.Title ? 1 : -1);   
+// };
 
-window.sorData = sorData; 
+// window.sorData = sorData; 
+
+  const compareSortData = (elemA, elemB) => {
+    if (elemA.Title > elemB.Title)
+      return 1;
+    if (elemA.Title < elemB.Title)
+      return -1;
+    return 0;
+  };
+  const sortData = (data, sortBy) => {
+    let sortedData = data.sort(compareSortData);
+    if (sortBy === "Az") {
+      return sortedData;
+    } else if (sortBy === "Za") {
+      return sortedData.reverse();
+    }
+    return data;
+  }
+  window.sortData = sortData;
+
 
--- src/main.js
+++ src/main.js
@@ -1,36 +1,40 @@
 // document.getElementById('getPosts').addEventListener('click', getPosts);
 const cardsContainer = document.getElementById('cards');
 const filterOutputMovies = document.getElementById('output');
+const botonAZ = document.getElementById("AzButton");
+const botonZA = document.getElementById("ZaButton");
+const buscarMovies = document.getElementById("send");
+const textoMovies = document.getElementById("text");
 
 
-const getPosts = () => {
-  fetch('http://www.omdbapi.com/?s=drama&&apikey=ee879aa1')
-    .then((res) => res.json())
-    .then((data) => {
-      allMovies(data.Search);
-      sortDataTotal(data);
-    });
-};
-getPosts();
+// const getPosts = () => {
+//   fetch('http://www.omdbapi.com/?s=drama&&apikey=ee879aa1')
+//     .then((res) => res.json())
+//     .then((data) => {
+//       allMovies(data.Search);
+//     });
+// };
+// getPosts();
 
-const allMovies = (data) => {
-  const displayCards = (data) => { /* mostrar los datos  en pantalla*/
-    let output = '<h2>Posts</h2>';
-    data.forEach(post => {
-      output += `
-      <div >
-        <h3>${post.Title}</h3>
-        <img src="${post.Poster}" alt="imagen de ${post.Title}">
-        <p>Type : ${post.Type}</p>   
-        <p>Year : ${post.Year}</p>         
-      </div>
-    `;
-    });
-    return cardsContainer.innerHTML = output;
-  };
-  displayCards(data);  
-};
+// const allMovies = (data) => {
+//   const displayCards = (data) => { /* mostrar los datos  en pantalla*/
+//     let output = '<h2>Posts</h2>';
+//     data.forEach(post => {
+//       output += `
+//       <div >
+//         <h3>${post.Title}</h3>
+//         <img src="${post.Poster}" alt="imagen de ${post.Title}">
+//         <p>Type : ${post.Type}</p>   
+//         <p>Year : ${post.Year}</p>         
+//       </div>
+//     `;
+//     });
+//     return cardsContainer.innerHTML = output;
+//   };
+//   displayCards(data);  
+// }
 
+let allMovies = [];
 
 const filterByTitle = (title) => {
   fetch(`https://www.omdbapi.com/?s=${title}&apikey=ea8492c7 `) 
@@ -38,7 +42,9 @@ const filterByTitle = (title) => {
       resp.json())
     .then((data) => {
       let string = ' ';
-      data.Search.forEach(post => {
+      allMovies = data.Search;
+      allMovies.forEach(post => {
+        filterOutputMovies.innerHTML = '';
         string += `
           <div >
             <h3>${post.Title}</h3>
@@ -47,28 +53,51 @@ const filterByTitle = (title) => {
             <p>Año : ${post.Year}</p>   
           </div>
         `;
+        
       });
-      return filterOutputMovies.innerHTML = string;
+      filterOutputMovies.innerHTML = string;
     });
 };
-filterByTitle('terror'); 
 
-const sortDataTotal = (data) => {
-  const compareSortData = (elemA, elemB) => {
-    if (elemA.name > elemB.name)
-      return 1;
-    if (elemA.name < elemB.name)
-      return -1;
-    return 0;
-  };
-  const sortBy = 'Za'
-  const sortData = (data, sortBy) => {
-    let sortedData = data.sort(compareSortData);
-    if (sortBy === "Az") {
-      return sortedData;
-    } else if (sortBy === "Za") {
-      return sortedData.reverse();
-    }
-    return data;
-  }
-}
+filterByTitle('drama');
+buscarMovies.addEventListener("click", () => {
+  
+  let valueImput =textoMovies.value
+  filterByTitle(valueImput); 
+})
+
+botonAZ.addEventListener("click", () => {
+  filterOutputMovies.innerHTML = '';
+  let string = ' ';
+  let ordenado = sortData(allMovies,"Az");
+  ordenado.forEach(post => {
+    string += `
+      <div >
+        <h3>${post.Title}</h3>
+        <img src="${post.Poster}" alt="imagen de ${post.Title}">
+        <p>Tipo : ${post.Type}</p>   
+        <p>Año : ${post.Year}</p>   
+      </div>
+    `;
+    
+  });
+  filterOutputMovies.innerHTML = string;
+});
+
+botonZA.addEventListener("click", () => {
+  filterOutputMovies.innerHTML = '';
+  let string = ' ';
+  let ordenadoTwo = sortData(allMovies,"Za");
+  ordenadoTwo.forEach(post => {
+    string += `
+      <div >
+        <h3>${post.Title}</h3>
+        <img src="${post.Poster}" alt="imagen de ${post.Title}">
+        <p>Tipo : ${post.Type}</p>   
+        <p>Año : ${post.Year}</p>   
+      </div>
+    `;
+    
+  });
+  filterOutputMovies.innerHTML = string;
+})
